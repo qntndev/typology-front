58113b19daba8d59beb6554f54c52fb33b7d09a8
Quentin <quentin@vingtcinq.io>
2019-11-06 20:43:14 +0100

    add meta tags to prevent indexation
    noindex, nofollow

--- src/main.js
+++ src/main.js
@@ -8,4 +8,10 @@ import './assets/css/global.scss';
 export default function(Vue, { router, head, isClient }) {
 	// Set default layout as a global component
 	Vue.component('Layout', DefaultLayout);
+
+	// Add a meta tag
+	head.meta.push({
+		name: 'robots',
+		content: 'noindex, nofollow'
+	});
 }
